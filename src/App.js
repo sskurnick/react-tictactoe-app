0371ef954c45031b986dc18869f9b00de578f2de
Samantha Skurnick <sa286446@ucf.edu>
2020-02-19 11:51:27 -0500

    #1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './components/TicTacToe/index.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Login extends React.Component {
+
+  clickHandler() {
+    console.log("User is logging on");
+    console.log(this);
+    for(const user of this.props.users){
+    if(this.refs.username.value === user.username && 
+      this.refs.password.value === user.password)
+      this.props.logInUser();
+   } 
+  }
+
+  render() {
+    return <div>
+      <input ref="username" type="text" />
+      <input ref="password" type="text" />
+      <input type="button" value="Login"  onClick={() => this.clickHandler()}/>
+    </div>;
+    }
+  }
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {loggedIn:false,
+      users:[{username:"test", password:"test"}]
+    }
+  }
+
+  logInUser(loggedInStatus) {
+    this.setState({loggedIn:loggedInStatus});
+  }
+
+  render() {
+    let returnComponent;
+    const login = <Login users={this.state.users}
+    logInUser={() => this.logInUser(true)} />;
+    const game = <Game logOutUser={() => this.logInUser(false)} />;
+  
+   if(!this.state.loggedIn) {
+     returnComponent = login;
+   } else {
+     returnComponent = game;
+   }
+   return returnComponent;
+  }
 }
 
 export default App;
