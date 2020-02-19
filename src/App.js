b5b9296318d86125998788dea5eb2f1d35705167
Samantha Skurnick <sa286446@ucf.edu>
2020-02-19 12:13:27 -0500

    #1

--- src/App.js
+++ src/App.js
@@ -3,27 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Game from './components/TicTacToe/index.js'
 
-class Login extends React.Component {
-
-  clickHandler() {
-    console.log("User is logging on");
-    console.log(this);
-    for(const user of this.props.users){
-    if(this.refs.username.value === user.username && 
-      this.refs.password.value === user.password)
-      this.props.logInUser();
-   } 
-  }
-  
-
-  render() {
-    return <div>
-      <input ref="username" type="text" />
-      <input ref="password" type="text" />
-      <input type="button" value="Login"  onClick={() => this.clickHandler()}/>
-    </div>;
-    }
-  }
 
 class App extends React.Component {
 
@@ -39,17 +18,16 @@ class App extends React.Component {
   }
 
   render() {
-    let returnComponent;
-    const login = <Login users={this.state.users}
-    logInUser={() => this.logInUser(true)} />;
-    const game = <Game logOutUser={() => this.logInUser(false)} />;
-  
-   if(!this.state.loggedIn) {
-     returnComponent = login;
-   } else {
-     returnComponent = game;
-   }
-   return returnComponent;
+   return (
+     (!this.state.register) ?
+     <Register />
+     :
+     (!this.state.loggedIn) ?
+     <Login users={this.state.users}
+      logInUser={() => this.logInUser(true)} />
+      :
+      <Game logOutUser={() => this.logInUser(false)} />
+   )
   }
 }
 
---
+++ src/components/login/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+    clickHandler() {
+      console.log("User is logging on");
+      console.log(this);
+      for(const user of this.props.users){
+      if(this.refs.username.value === user.username && 
+        this.refs.password.value === user.password)
+        this.props.logInUser();
+     } 
+    }
+    
+  
+    render() {
+      return <div>
+        <input ref="username" type="text" />
+        <input ref="password" type="text" />
+        <input type="button" value="Login"  onClick={() => this.clickHandler()}/>
+      </div>;
+      }
+    }
+  
